e141b085ee02a9c099a9e3c2992d6e20b18c4aa0
Chen Xiao <ListenToATree@gmail.com>
2022-05-29 12:58:53 +0800

    5-29

---
+++ shared/set.go
@@ -0,0 +1,49 @@
+package shared
+
+// Set is a multi set data structure.
+type Set[T comparable] struct {
+	items map[T]int
+}
+
+// NewSet returns a new Set.
+func NewSet[T comparable]() *Set[T] {
+	return &Set[T]{
+		items: make(map[T]int),
+	}
+}
+
+// Has returns whether the Set has
+// a value (true) or not (false).
+func (s *Set[T]) Has(value T) bool {
+	_, exists := s.items[value]
+	return exists
+}
+
+// Get returns the occurrence of the value.
+func (s *Set[T]) Get(value T) int {
+	occurrence, exists := s.items[value]
+	if !exists {
+		return -1
+	}
+	return occurrence
+}
+
+// Add adds a value in the Set.
+func (s *Set[T]) Add(value T) {
+	if s.Has(value) {
+		s.items[value]++
+	} else {
+		s.items[value] = 1
+	}
+}
+
+// Remove removes a value from the Set.
+func (s *Set[T]) Remove(value int) {
+	if occurrence, exists := s.items[value]; exists {
+		if occurrence == 1 {
+			delete(s.items, value)
+		} else {
+			s.items[value]--
+		}
+	}
+}
---
+++ tree/twosum4.go
@@ -0,0 +1,27 @@
+package tree
+
+import (
+	"unstoppable-leetcode/shared"
+)
+
+type BSTNode struct {
+	Val   int
+	Left  *BSTNode
+	Right *BSTNode
+}
+
+func findTarget(root *BSTNode, k int) bool {
+	set := shared.NewSet[int]()
+	return find(root, k, set)
+}
+
+func find(root *BSTNode, k int, set *shared.Set[int]) bool {
+	if root == nil {
+		return false
+	}
+	if set.Has(k - root.Val) {
+		return true
+	}
+	set.Add(root.Val)
+	return find(root.Left, k, set) || find(root.Right, k, set)
+}
